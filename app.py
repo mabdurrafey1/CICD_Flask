9cd23c893788df7eb09b78414e09a6dddc7372ec
Muhammad Abdur Rafey Ahmed <80873403+mabdurrafey1@users.noreply.github.com>
2023-03-30 22:19:52 +0500

    updated code

--- app.py
+++ app.py
@@ -5,10 +5,10 @@ app = Flask(__name__)
 @app.route('/check_overlap', methods=['POST'])
 def check_overlap():
     data = request.get_json()
-    range1_start = data['range1_start']
-    range1_end = data['range1_end']
-    range2_start = data['range2_start']
-    range2_end = data['range2_end']
+    range1_start = data['range1']['start']
+    range1_end = data['range1']['end']
+    range2_start = data['range2']['start']
+    range2_end = data['range2']['end']
 
     status= range1_start < range2_end and range1_end > range2_start
     
